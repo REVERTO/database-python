a943822308b9bfd0ae2599b8e3f6442d309d8e4c
Ito Tomoyuki <ito@nagisa-inc.jp>
2017-03-03 15:11:08 +0900

    fix

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 if bool(os.environ.get('LOCAL_DEV', False)):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+    print 'env prod'
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    print 'env local'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
