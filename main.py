6241339acbcebcd874201d889161bca08b09d5af
Ito Tomoyuki <ito@nagisa-inc.jp>
2017-03-03 14:22:04 +0900

    fix

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Setting database URL
-if bool(os.environ.get('LOCAL_DEV', True)):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+# if bool(os.environ.get('LOCAL_DEV', True)):
+#     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# else:
+#     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
